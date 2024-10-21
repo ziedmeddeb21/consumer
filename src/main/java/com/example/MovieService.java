0b37d25b44a6ba9562f0290d517729227b0fac11
Zied MEDDEB <zied.meddeb@teamwillgroup.com>
2024-10-21 17:22:16 +0100

    reversed mapping logic

--- src/main/java/com/example/MovieService.java
+++ src/main/java/com/example/MovieService.java
@@ -30,34 +30,21 @@ public class MovieService {
         // Create a new JSON object to store the transformed movie
         ObjectNode transformedMovie = objectMapper.createObjectNode();
 
-        // Iterate through JSON attributes
-        Iterator<Map.Entry<String, JsonNode>> fields = movieJson.fields();
-        while (fields.hasNext()) {
-            Map.Entry<String, JsonNode> field = fields.next();
-            String sourceField = field.getKey();
-            JsonNode value = field.getValue();
-            System.out.println("sourceField: " + sourceField + " value: " + value);
+        // Iterate through mapping rules
+        for (Document mappingRule : allMappingRules) {
+            String sourceField = mappingRule.getString("source_field");
+            String targetField = mappingRule.getString("target_field");
 
+            // Check if the source field exists in the JSON object
+            if (movieJson.has(sourceField)) {
 
-//             Find the corresponding mapping rule
-            for (Document mappingRule : allMappingRules) {
-                if (mappingRule.getString("source_field").equals(sourceField)) {
-                    String targetField = mappingRule.getString("target_field");
-                    transformedMovie.set(targetField, value);
-                    System.out.println("targetField: " + targetField + " value: " + value);
-                    System.out.println("Transformed Movie: " + transformedMovie);
-                    break;
-                }
+                JsonNode value = movieJson.get(sourceField);
+                transformedMovie.set(targetField, value);
+                System.out.println("sourceField: " + sourceField + " targetField: " + targetField + " value: " + value);
             }
-
         }
-        movieRepository.addMovie(transformedMovie);
 
-
-
-//
-//        System.out.println("Transformed Movie: " + transformedMoive);
         // Add the transformed movie to the repository
-
+        movieRepository.addMovie(transformedMovie);
     }
 }
