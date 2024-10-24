716417ff1bf0664bc931f5dc8590a816eaba1f30
Zied MEDDEB <zied.meddeb@teamwillgroup.com>
2024-10-24 09:48:58 +0100

    key value working

--- src/main/java/com/example/MappingRule.java
+++ src/main/java/com/example/MappingRule.java
@@ -6,14 +6,16 @@ public class MappingRule {
     public String source_field;
     public String target_field;
     public boolean isArray;
+    public boolean isKeyVal;
 
 
-    public MappingRule(String field_type, String source_field, String target_field, boolean isArray){
+    public MappingRule(String field_type, String source_field, String target_field, boolean isArray, boolean isKeyVal) {
         this.field_type = field_type;
 
         this.source_field = source_field;
         this.target_field = target_field;
         this.isArray = isArray;
+        this.isKeyVal = isKeyVal;
     }
 
     public MappingRule(){
--- src/main/java/com/example/MovieService.java
+++ src/main/java/com/example/MovieService.java
@@ -30,14 +30,52 @@ public class MovieService {
 
 //         Create a new JSON object to store the transformed movie
         ObjectNode transformedMovie = objectMapper.createObjectNode();
+
+        ArrayNode arrayKeyVal = objectMapper.createArrayNode();
 //        // Iterate through mapping rules
         for (Document mappingRule : allMappingRules) {
             String sourceField = mappingRule.getString("source_field");
             String targetField = mappingRule.getString("target_field");
+            if (mappingRule.getBoolean("isKeyVal")) {
+                //get the key and the value from the source
+                String key = sourceField.substring(sourceField.lastIndexOf('/') + 1);
+                JsonNode value = movieJson.at(sourceField);
+                System.out.println("Key: " + key);
+                System.out.println("Value: " + value);
+                ObjectNode targetJson = objectMapper.createObjectNode();
+                targetJson.put("key", key);
+                targetJson.put("value", value);
+                System.out.println("Target Json: " + targetJson);
+
+                arrayKeyVal.add(targetJson);
+                System.out.println("Target array: " + arrayKeyVal);
+
+
+                // Split the target field path into components
+                String[] targetPathComponents = targetField.split("/");
+                // Navigate or create the necessary nodes in the transformedMovie
+                //example /inf/metadata/title      Note: the one before the last element is the array name
+                ObjectNode currentNode = transformedMovie;
+                for (int i = 1; i < targetPathComponents.length - 1; i++) {
+                    //if we reach the array name we stop the loop
+                    if(i == targetPathComponents.length - 2){
+                        break;
+                    }
+                    String pathComponent = targetPathComponents[i];
+                    if (!currentNode.has(pathComponent)) {
+                        currentNode.set(pathComponent, objectMapper.createObjectNode());
+                    }
+                    currentNode = (ObjectNode) currentNode.get(pathComponent);
+                }
+                //we create lastly the array and add the key value pair elements
+                currentNode.set(targetPathComponents[targetPathComponents.length - 2], arrayKeyVal);
+
+
+
+            }
+            else{
             //if the source field is in an array example messages :[{"from":"me","to":"you"}]
             if (mappingRule.getBoolean("isArray")) {
-                //get the array name and the field name from the source
-                String arrayName ="/"+ sourceField.split("/")[sourceField.split("/").length - 2];
                 String fieldName = sourceField.split("/")[sourceField.split("/").length - 1];
                 // get the target field name
                 String targetFieldName = targetField.split("/")[targetField.split("/").length - 1];
@@ -50,7 +88,7 @@ public class MovieService {
                 ArrayNode newArray = objectMapper.createArrayNode();
                 //we loop through the array of objects using the current node to get the value of the object
                 ObjectNode currentNode = objectMapper.createObjectNode();
-                for(int i=0; i< array.size();i++ ){
+                for (int i = 0; i < array.size(); i++) {
                     currentNode = (ObjectNode) array.get(i);
                     //get the value of the field name like the value of "from"
                     JsonNode value = currentNode.get(fieldName);
@@ -64,20 +102,20 @@ public class MovieService {
                 }
 
                 String[] targetPathComponents = targetField.substring(0, targetField.lastIndexOf('/')).split("/");
-
-                if(targetPathComponents.length == 1){
+                //if the target field is at the root of the object
+                if (targetPathComponents.length == 1) {
                     transformedMovie.set(targetPathComponents[0], newArray);
                 }
-                else{
+                //else navigate to the target field and add the new array
+                else {
                     ObjectNode navigatedNode = transformedMovie;
-                    for(int i = 1; i< targetPathComponents.length -1; i++){
+                    for (int i = 1; i < targetPathComponents.length - 1; i++) {
                         String pathComponent = targetPathComponents[i];
                         navigatedNode = (ObjectNode) navigatedNode.get(pathComponent);
                     }
-                    String destinationArrayName =  targetField.split("/")[targetField.split("/").length - 2];
-                    navigatedNode.set(destinationArrayName,newArray);
+                    String destinationArrayName = targetField.split("/")[targetField.split("/").length - 2];
+                    navigatedNode.set(destinationArrayName, newArray);
                 }
-
 
 
             } else {
@@ -96,19 +134,20 @@ public class MovieService {
                 // Set the value at the correct location
 
                 //if it's an array then initizalize it as an empty array
-                if(mappingRule.getString("field_type").equals("Array")){
+                if (mappingRule.getString("field_type").equals("Array")) {
                     String finalPathComponent = targetPathComponents[targetPathComponents.length - 1];
                     ArrayNode emptyArray = objectMapper.createArrayNode();
                     currentNode.set(finalPathComponent, emptyArray);
 
                 }
                 //if it's a normal node then set the value
-                else{
+                else {
                     String finalPathComponent = targetPathComponents[targetPathComponents.length - 1];
                     currentNode.set(finalPathComponent, movieJson.at(sourceField));
                 }
 
             }
+        }
         }
 
 
